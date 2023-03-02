091c504f900963ec56570c0a5842f9a9458bb324
whale <yalemac97@gmail.com>
2023-03-02 18:33:31 +1100

    random character generating but not looping

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,17 @@ var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
 var numericNum = ["0","1","2","3","4","5","6","7","8","9",];
 var specialCharactersOptions = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+",];
 var passwordLength = 8 || 128;
-var avaliable = [];
+var avaliable =[];
 
 
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  var completedPassword = document.querySelector("#password");
+  
 }
 
 generateBtn.addEventListener("click", writePassword);
+
 
 function generatePassword() {
   var passwordLength = prompt ("How many characters would you like your password to contain? Type a number between 8 and 128")
@@ -31,43 +32,42 @@ return null;
 
 var lowerCase = window.confirm ('Would you like to use lower case letters in your password? eg. abcdefghijklmnopqrstuvwxyz');
   if (lowerCase == true) {
-    avaliable = avaliable.concat (lowerCaseLetters)
+    avaliable = (avaliable || []).concat(lowerCaseLetters);
   } function lowerCase(){
   return lowerCase[Math.floor(Math.random()* lowerCase.length)];
   }console.log(lowerCaseLetters);
 
 var upperCase = window.confirm ('Would you like to use upper case letters in your password? eg. ABCDEFGHIJKLMNOPQRSTUVWXYZ');
   if (upperCase == true) {
-    avaliable = avaliable.concat (upperCaseLetters)
+    avaliable = avaliable.concat (upperCaseLetters);
   } function upperCase(){
   return upperCase[Math.floor(Math.random() * upperCase.length)];
   }console.log(upperCaseLetters);
 
   var numeric= window.confirm ('Would you like to use numbers in your password? eg. 1234567890');
   if (numeric == true) {
-    avaliable = avaliable.concat (numericNum)
+    avaliable = avaliable.concat (numericNum);
   } function numeric(){
   return numeric[Math.floor(Math.random() * numeric.length)];
   }console.log(numericNum);
 
   var specialCharacters= window.confirm ('Would you like to use numbers in your password? eg. !@##$%^&*()-_=+');
   if (specialCharacters == true) {
-    avaliable = avaliable.concat (specialCharactersOptions)
+    avaliable = avaliable.concat (specialCharactersOptions);
   } function specialCharacters(){
   return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
   }console.log(numericNum);
+  
 
-  for (var i = 0; i < length; i++){
-  var password = getRandom(avaliable)
+  for (var i = 0; i < passwordLength; i++){
+    var completedPassword = getRandom(avaliable)
+  
+  
+    function getRandom(arr) {
+      var randIndex = Math.floor(Math.random() * arr.length);
+      var randElement = arr[randIndex];
+  
+      return randElement;
+    }
   }
-
-  function getRandom(arr) {
-    var randIndex =Math.floor(math.random() * arr.length);
-    var randElement = arr[randIndex];
-
-    return randElement;
-  } console.log(randElement);
-} 
-
-
-
+}
